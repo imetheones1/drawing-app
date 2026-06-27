063ba52b5198b711d40f84ad44325ba3e745e147
imetheones1 <65839659+imetheones1@users.noreply.github.com>
2026-06-27 16:43:57 -0700

    use appstate

--- main.c
+++ main.c
@@ -6,24 +6,33 @@
 #define ARENA_IMPLEMENTATION
 #include "include/arena.h"
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+typedef struct AppState {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    size_t debug;
+} AppState;
 
-// static SDL_Surface *test_surface = NULL;
 static SDL_Texture *test_texture = NULL;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
-    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    AppState *state = SDL_malloc(sizeof(AppState));
+
+    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_RESIZABLE, &(state->window), &(state->renderer))) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     SDL_Surface *test_surface = loadAsset(asset_please,asset_please_len);
-    test_texture = SDL_CreateTextureFromSurface(renderer,test_surface);
+    test_texture = SDL_CreateTextureFromSurface(state->renderer,test_surface);
     SDL_DestroySurface(test_surface);
+
+    state->debug = 0;
+
+    *appstate = state;
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
+    AppState* state = (AppState*)appstate;
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
@@ -31,27 +40,30 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate){
-    // const char *message = "Hello World!";
-    // int w = 0, h = 0;
-    // float x, y;
-    // const float scale = 4.0f;
+    AppState* state = (AppState*)appstate;
+    int w = 0, h = 0;
+    SDL_GetCurrentRenderOutputSize(state->renderer, &w, &h);
 
-    // SDL_GetCurrentRenderOutputSize(renderer, &w, &h);
-    // SDL_SetRenderScale(renderer, scale, scale);
-    // x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    // y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+    float x, y;
 
-    // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    // SDL_RenderClear(renderer);
-    // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    // SDL_RenderDebugText(renderer, x, y, message);
+    x = (w/2);
+    y = ((h) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
-    SDL_RenderTexture(renderer, test_texture, NULL, NULL);
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
+    SDL_RenderClear(state->renderer);
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderTexture(state->renderer, test_texture, NULL, NULL);
+    SDL_RenderDebugTextFormat(state->renderer, x, y, "%zu",state->debug);
+
+
+    SDL_RenderPresent(state->renderer);
+
+    state->debug++;
 
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result){
+    AppState* state = (AppState*)appstate;
+
 }
